3af72ac3a29c65d4707dda5d68e8ef7e638840ea
vibraniumforge <gregm9999@gmail.com>
2020-01-29 13:39:45 -0800

    added a sentence

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
           An app with a Ruby on Rails backend and a React and Redux front end.
           This app simulates a train board at your station of choosing. It can
           also save and display trains that you enter on your own train board.
-          Some seeds included.
+          Some seeds included. Choose one of the options above to begin.
         </p>
       </div>
     );
