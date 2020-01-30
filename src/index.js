933be646d791cb6755cdf578ac1e558dedc37db1
vibraniumforge <gregm9999@gmail.com>
2020-01-29 19:23:12 -0800

    removed redux devtools

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+// import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/index";
 import "./index.css";
 import App from "./App";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 // The Redux dev tools throw an error here.
 // If the program does not work comment OUT the above line and comment IN the below line.
 // and be sure to comment in the import statement above.
 
 // also remove to work with non-chrome browsers
-
-// const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
