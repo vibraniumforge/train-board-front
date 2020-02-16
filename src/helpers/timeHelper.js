97af96187fb1a4cbe38b9e807aa8f1f60c413c48
vibraniumforge <gregm9999@gmail.com>
2020-02-16 15:31:46 -0800

    now only 12h time with am/pm

--- src/helpers/timeHelper.js
+++ src/helpers/timeHelper.js
@@ -1,7 +1,16 @@
-const timeHelper = time => {
+const timeHelper = (time, time24) => {
   const newTime = time ? time.toString().trim() : null;
-  if (newTime) {
-    return `${newTime.slice(0, 2)}:${newTime.slice(2)}`;
+  const time24Integer = parseInt(time24, 10);
+  if (newTime && time24Integer) {
+    if (time24Integer < 1200) {
+      if (newTime.charAt(0) === "0") {
+        return `${newTime.slice(1, 2)}:${newTime.slice(2)}`;
+      } else {
+        return `${newTime.slice(0, 2)}:${newTime.slice(2)}`;
+      }
+    } else {
+      return `${newTime.slice(0, 2)}:${newTime.slice(2)} p`;
+    }
   } else {
     return null;
   }
