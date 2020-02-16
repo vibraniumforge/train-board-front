bcec1a81d7db1da9455986c061b2b52b93d6491f
vibraniumforge <gregm9999@gmail.com>
2020-02-16 15:30:54 -0800

    combined home and about

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
-import About from "./components/About";
 import Home from "./components/Home";
 import AmtrakStationSearch from "./containers/AmtrakStationSearch";
 import AmtrakStationSelect from "./containers/AmtrakStationSelect";
@@ -17,12 +16,11 @@ export default (
       <Navbar />
       <Switch id="routes">
         <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
         <Route path="/search_amtrak_station" component={AmtrakStationSearch} />
         <Route path="/select_amtrak_station" component={AmtrakStationSelect} />
+        <Route path="/view_user_trains" component={ViewUserTrains} />
         <Route path="/new_user_train" component={NewTrainForm} />
         <Route path="/edit_user_train" component={EditTrainForm} />
-        <Route path="/view_user_trains" component={ViewUserTrains} />
       </Switch>
     </div>
   </Router>
