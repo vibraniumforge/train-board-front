e6e7b77cba41c45089a2f5e74a572944d0a9118c
vibraniumforge <gregm9999@gmail.com>
2020-02-16 15:35:46 -0800

    merge with about

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,31 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="explanation">
         <p>
           An app with a Ruby on Rails backend and a React and Redux front end.
           This app simulates a train board at your station of choosing. It can
           also save and display trains that you enter on your own train board.
-          Some seeds included. Choose one of the options above to begin.
+          Some seeds are included.
         </p>
+        <p>
+          <strong>Select an Amtrak Station</strong> - This board is designed to
+          track arrivals and departures from 10 of the top Amtrak stations in
+          the U.S.A. Use the dropdown to select a station.
+        </p>
+        <p>
+          <strong>Search for an Amtrak Station</strong> - Search for a station.
+          You can choose to view its board from there.
+        </p>
+        <p>
+          <strong>Create a new Train</strong> - Create your own train. A form
+          appears where you can enter information to save.
+        </p>
+        <p>
+          <strong>View My Trains</strong> - Click here to view your existing
+          trains.
+        </p>
+        <p>Choose one of the options above to begin!</p>
       </div>
     );
   }
