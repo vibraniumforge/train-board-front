d97850da23849a0173f83de6c2871bfe76f664b3
vibraniumforge <gregm9999@gmail.com>
2020-02-16 15:33:00 -0800

    removed class to another helper

--- src/helpers/remarksHelper.js
+++ src/helpers/remarksHelper.js
@@ -1,17 +1,11 @@
 const remarksHelper = remark => {
   const trimmedRemark = remark.trim();
-  if (trimmedRemark.indexOf("E") > -1) {
-    return "green";
-  }
+
   switch (trimmedRemark) {
-    case "On Time":
-      return "green";
-    case "Boarding":
-      return "green boarding";
-    case "Arrived":
-      return "green";
+    case "S. Disrupt":
+      return "Service Disrupt";
     default:
-      return "red";
+      return trimmedRemark;
   }
 };
 
